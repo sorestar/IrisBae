0fef9b5a5681fcad6df5e6a8dc175cc4b52c1a47
Iris <hyunjibae@gmail.com>
2026-06-16 21:21:25 +0900

    Delete projectscript.js

--- projectscript.js
+++
@@ -1,5 +0,0 @@
-
-(() => {
-    const copyrightElement = document.getElementById("copyright");
-    copyrightElement.innerHTML = "&copy;  " + new Date().getFullYear() + " https://sorestar.github.io/portfolio/ - All Rights Reserved.";
-})();
