8e765f1ff07bed6501c2964a1462d650b6791f3c
Iris <hyunjibae@gmail.com>
2026-06-17 16:39:11 +0900

    minor fixes - positioning

--- projectscript.js
+++ projectscript.js
@@ -1,4 +1,11 @@
 
+jQuery(document).ready(function ($) {
+    $('.slider-img').on('click', function () {
+        $('.slider-img').removeClass('active');
+        $(this).addClass('active');
+    })
+});
+
 (() => {
     const copyrightElement = document.getElementById("copyright");
     copyrightElement.innerHTML = "&copy;  " + new Date().getFullYear() + " https://sorestar.github.io/portfolio/ - All Rights Reserved.";
