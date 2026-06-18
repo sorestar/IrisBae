083ae39fd01b094d2899699750349444258ca215
Iris <hyunjibae@gmail.com>
2026-06-18 11:42:44 +0900

    testing color changing on elements

--- projectscript.js
+++ projectscript.js
@@ -1,9 +1,36 @@
 
 jQuery(document).ready(function ($) {
     $('.slider-img').on('click', function () {
-        $('.slider-img').removeClass('active');
-        $(this).addClass('active');
-    })
+        //thisOne 은 열리는거
+        const allOnes = $('.slider-img');
+        const thisOne = $(this);
+        const thisId = this.id;
+        allOnes.css('background-color', 'grey');
+        console.log(thisId);
+        if(thisId == "lights") {
+            //thisOne.css('background-image', "url('images/favicon.png')");
+            thisOne.css('background-color', 'red');
+        }
+        else if(thisId == "shadow") {
+            thisOne.css('background-color', 'orange');
+
+        }
+        else if(thisId == "mask") {
+            thisOne.css('background-color', 'black');
+
+        }
+        else if(thisId == "cafeteria") {
+            thisOne.css('background-color', 'green');
+
+        }
+        else if(thisId == "java") {
+            thisOne.css('background-color', 'blue');
+
+        }
+        
+        allOnes.removeClass('active');
+        thisOne.addClass('active');
+    });
 });
 
 (() => {
