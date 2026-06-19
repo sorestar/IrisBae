549c3bacd2e77d17342699cea411ee9c561f1025
Iris <hyunjibae@gmail.com>
2026-06-19 19:08:32 +0900

    making slider work / resize properly

--- projectscript.js
+++ projectscript.js
@@ -16,15 +16,15 @@ jQuery(document).ready(function ($) {
 
         }
         else if(thisId == "mask") {
-            thisOne.css('background-color', 'black');
-
-        }
-        else if(thisId == "cafeteria") {
             thisOne.css('background-color', 'green');
 
         }
-        else if(thisId == "java") {
+        else if(thisId == "cafeteria") {
             thisOne.css('background-color', 'blue');
+
+        }
+        else if(thisId == "java") {
+            thisOne.css('background-color', 'purple');
 
         }
         
@@ -32,8 +32,55 @@ jQuery(document).ready(function ($) {
         thisOne.addClass('active');
     });
 });
-
 (() => {
     const copyrightElement = document.getElementById("copyright");
     copyrightElement.innerHTML = "&copy;  " + new Date().getFullYear() + " https://sorestar.github.io/portfolio/ - All Rights Reserved.";
 })();
+new Swiper('.card-wrapper', {
+    loop:true,
+
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+            
+        },
+        760: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+});
+/*
+window.onresize = function() {
+    const width = window.innerWidth;
+        const cardList = document.querySelector('.card-list');
+    if(width < 760) {
+        cardList.style.marginLeft = "15vw";
+        console.log("did");
+    }
+    else if (width > 760) {
+        cardList.style.marginLeft = "39px";
+
+    }
+    
+};
+const width = window.innerWidth;
+    if(width < 760) {
+        const cardList = document.querySelector('.card-list');
+        cardList.style.marginLeft = "18vw";
+        console.log("did");
+    }
+    */
