055dd81a52846119bf36394561d25cc66c8a32ca
Iris <hyunjibae@gmail.com>
2026-06-22 15:31:51 +0900

    add language switching , decorated cards

--- projectscript.js
+++ projectscript.js
@@ -7,27 +7,27 @@ jQuery(document).ready(function ($) {
         const thisId = this.id;
         allOnes.css('background-color', 'grey');
         console.log(thisId);
-        if(thisId == "lights") {
+        if (thisId == "lights") {
             //thisOne.css('background-image', "url('images/favicon.png')");
             thisOne.css('background-color', 'red');
         }
-        else if(thisId == "shadow") {
+        else if (thisId == "shadow") {
             thisOne.css('background-color', 'orange');
 
         }
-        else if(thisId == "mask") {
+        else if (thisId == "mask") {
             thisOne.css('background-color', 'green');
 
         }
-        else if(thisId == "cafeteria") {
+        else if (thisId == "cafeteria") {
             thisOne.css('background-color', 'blue');
 
         }
-        else if(thisId == "java") {
+        else if (thisId == "java") {
             thisOne.css('background-color', 'purple');
 
         }
-        
+
         allOnes.removeClass('active');
         thisOne.addClass('active');
     });
@@ -37,9 +37,15 @@ jQuery(document).ready(function ($) {
     copyrightElement.innerHTML = "&copy;  " + new Date().getFullYear() + " https://sorestar.github.io/portfolio/ - All Rights Reserved.";
 })();
 new Swiper('.card-wrapper', {
-    loop:true,
+    loop: true,
+    initialSlide: 1,
 
-    pagination:{
+    on: {
+        init: updateText, resize,
+        slideChange: updateText,
+    },
+
+    pagination: {
         el: '.swiper-pagination',
         clickable: true,
         dynamicBullets: true
@@ -53,7 +59,7 @@ new Swiper('.card-wrapper', {
     breakpoints: {
         0: {
             slidesPerView: 1
-            
+
         },
         760: {
             slidesPerView: 2
@@ -63,20 +69,59 @@ new Swiper('.card-wrapper', {
         },
     }
 });
-/*
-window.onresize = function() {
-    const width = window.innerWidth;
-        const cardList = document.querySelector('.card-list');
-    if(width < 760) {
-        cardList.style.marginLeft = "15vw";
-        console.log("did");
+ document.getElementById("korButton").addEventListener('click', () => {
+        window.location.reload();
+    });
+
+     document.getElementById("engButton").addEventListener('click', () => {
+        window.location.reload();
+    });
+
+
+function updateText() {
+    const lang = localStorage.getItem("lang") || "eng";
+    let captions;
+    if(lang == "eng"){
+
+     captions = ['', 'this is my wacky java game.', 'this is a mask thing', 'this is my shadow', "Co-coordinator of the LightsUp Conference<br> held by the Sustainable Energy Club at Purdue University<br>"
+        + "Focus on Outreach & VR Room<br>"];
     }
-    else if (width > 760) {
+    else{
+         captions = ['', '자바게임.', '마스크 뭐시기', '그림자',"학회"];
+    }
+    document.querySelector('.projectText').innerHTML = captions[this.realIndex + 1];
+}
+function resize() {
+    const width = window.innerWidth;
+    const deco = document.getElementById('deco');
+    const cardList = document.querySelector('.card-list');
+    const dummy = document.querySelector('.dummy');
+    const announce = document.querySelector('.announce');
+    if (width < 760) {
+        deco.style.height = '0px';
+        dummy.style.height = '120vh';
+        announce.style.height = '46vh';
+        announce.style.fontSize = '15px';
+    } else if (width >= 760 && width < 1024) {
+        deco.style.height = '140px';
+        deco.style.marginLeft = '65%';
+        dummy.style.height = '0px';
+        announce.style.height = '0px';
+        announce.style.fontSize = '0px';
+        cardList.style.marginLeft = "70px";
+    }
+    else if (width >= 1024) {
+        deco.style.height = '140px';
+        deco.style.marginLeft = '44%';
+        dummy.style.height = '0px';
+        announce.style.height = '0px';
+        announce.style.fontSize = '0px';
         cardList.style.marginLeft = "39px";
 
     }
-    
-};
+}
+window.onresize = resize();
+/*
 const width = window.innerWidth;
     if(width < 760) {
         const cardList = document.querySelector('.card-list');
